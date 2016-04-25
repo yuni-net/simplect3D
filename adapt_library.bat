5d92d28b2d98a8daa972520d2effa1229216e2b0
yuni-net <yuni.net.liberty@gmail.com>
2016-04-25 13:56:13 +0900

    apply.bat

--- adapt_library.bat
+++
@@ -1,3 +0,0 @@
-copy simplect3D_project\Debug\simplect3D.lib simplect3D\simplect3Dd.lib
-copy simplect3D_project\Release\simplect3D.lib simplect3D\simplect3D.lib
-pause
---
+++ apply.bat
@@ -0,0 +1,5 @@
+echo applying...
+copy .\simplect3D_project\Debug\simplect3D.lib .\simplect3D\simplect3Dd.lib
+copy .\simplect3D_project\Release\simplect3D.lib .\simplect3D\simplect3D.lib
+echo finished!
+pause
